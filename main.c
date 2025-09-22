ec3cbe9071ee1aed6bc27b1c17780836f2bea21f
matasio9838 <matasiobright@gmail.com>
2025-09-22 18:20:24 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+//Name:Bright Atsiaya Matasio
+//Reg No:PA106/G/28751/25
+//Description:my programing work demonstrating basic fundamental programing in C coding;
+
+#include <stdio.h>
+
+int main()
+{   printf("SOFTWARE ENGINEERING STUDENT YEAR 1 UNITS \n");
+    printf("========================================================\n");
+    printf("SPC 2101:Introduction to Programming and Problem Solving \n");
+    printf("SPI 2102:Computer System and Organization\n");
+    printf("SPM 2100:Discreet Mathematics\n");
+    printf("SPM 2104:Mathematics for Science\n");
+    printf("SSE 2101:Fundamentals of System Analysis\n");
+    printf("UCU 2105:Fundamentals of ICT\n");
+    printf("UCU 2106:Communication and Information Skills\n");
+
+    printf("END OF SEMESTER 1 UNITS\n");
+
+    printf("========================================================\n");
+    char name [20];
+    char height [10];
+    char idNumber [10];
+    char bankBalance [50];
+
+    printf("enter name:");
+    scanf("%s",name);
+    printf("enter your height:");
+    scanf("%d",&height);
+    printf("enter idNumber:");
+    scanf("%d",&idNumber);
+    printf("enter bankBalance:");
+    scanf("%d",&bankBalance);
+
+    return 0;
+}
